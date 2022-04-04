953bf2eb20b2614531fe4dbb52645dc633eae14d
Pinball Power <info@pinball-power.com>
2022-04-04 11:28:50 +0200

    Moved file

---
+++ util/bithelper.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+
+uint8_t reverse_byte(uint8_t x);
+void reverse_byte_array(uint8_t* arr, int len);
+
+uint32_t reverse_word_order(uint32_t x);
+void reverse_word_order_array(uint32_t* arr, int len);
