8badda0b78279f79bd593adc2c7d88a4388a0c21
Pinball Power <info@pinball-power.com>
2022-04-21 12:20:30 +0200

    Split into .cpp/.h file

--- render/openglshader.h
+++ render/openglshader.h
@@ -19,91 +19,30 @@ class OpenGLShader
 public:
     unsigned int ID;
 
-    OpenGLShader() {
+    OpenGLShader();
+    OpenGLShader(const string vertexShader, const string fragmentShader);
 
-    }
-
-    OpenGLShader(const string vertexShader, const string fragmentShader)
-    {
-        compile_shaders(vertexShader, fragmentShader);
-    }
-
-    void compile_shaders(const string vertexShader, const string fragmentShader) {
-        const char* vShaderCode = vertexShader.c_str();
-        const char* fShaderCode = fragmentShader.c_str();
-        // 2. compile shaders
-        unsigned int vertex, fragment;
-        // vertex shader
-        vertex = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex, 1, &vShaderCode, NULL);
-        glCompileShader(vertex);
-        checkCompileErrors(vertex, "VERTEX");
-        // fragment Shader
-        fragment = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment, 1, &fShaderCode, NULL);
-        glCompileShader(fragment);
-        checkCompileErrors(fragment, "FRAGMENT");
-        // shader Program
-        ID = glCreateProgram();
-        glAttachShader(ID, vertex);
-        glAttachShader(ID, fragment);
-        glLinkProgram(ID);
-        checkCompileErrors(ID, "PROGRAM");
-        // delete the shaders as they're linked into our program now and no longer necessary
-        glDeleteShader(vertex);
-        glDeleteShader(fragment);
-    }
+    void compile_shaders(const string vertexShader, const string fragmentShader);
 
 
     // activate the shader
     // ------------------------------------------------------------------------
-    void use()
-    {
-        glUseProgram(ID);
-    }
+    void use();
+
     // utility uniform functions
     // ------------------------------------------------------------------------
-    void setBool(const std::string& name, bool value) const
-    {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-    }
+    void setBool(const std::string& name, bool value) const;
+
     // ------------------------------------------------------------------------
-    void setInt(const std::string& name, int value) const
-    {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-    }
+    void setInt(const std::string& name, int value) const;
+    
     // ------------------------------------------------------------------------
-    void setFloat(const std::string& name, float value) const
-    {
-        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-    }
+    void setFloat(const std::string& name, float value) const;
 
 private:
     // utility function for checking shader compilation/linking errors.
     // ------------------------------------------------------------------------
-    void checkCompileErrors(unsigned int shader, std::string type)
-    {
-        int success;
-        char infoLog[1024];
-        if (type != "PROGRAM")
-        {
-            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-            if (!success)
-            {
-                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-                BOOST_LOG_TRIVIAL(error) << "[shader] compilation error of type " << type;
-            }
-        }
-        else
-        {
-            glGetProgramiv(shader, GL_LINK_STATUS, &success);
-            if (!success)
-            {
-                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-                BOOST_LOG_TRIVIAL(error) << "[shader] linking error of type " << type;
-            }
-        }
-    }
+    void checkCompileErrors(unsigned int shader, std::string type); 
 };
 
 #endif
