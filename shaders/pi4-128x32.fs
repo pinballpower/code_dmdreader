ffb2b3b736e41d123d14cc4416292d3920328983
Pinball Power <info@pinball-power.com>
2022-04-22 11:51:33 +0200

    Fixed Pi4 shaders

--- shaders/pi4-128x32.fs
+++ shaders/pi4-128x32.fs
@@ -1,23 +1,25 @@
-// #version 300 es   
-// precision mediump float;
+#version 300 es
+precision mediump float;
 
-// out vec4 FragColor;
+// Input from vertex shader
+in vec3 v_color;
+in vec2 TexCoord;
 
-// 
-// varying vec2 TexCoord;
+// Output of fragment shader
+layout(location = 0) out vec4 fragColor;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2; // the texture that will be rendered on top of the DMD
 
-varying vec3 ourColor;
+// varying vec3 ourColor;
 
 void main()
 {
 	// Multiple pixel overlay
-	// vec2 tc2;
-	// tc2.x = TexCoord.x*128;
-	// tc2.y = TexCoord.y*128;
+	vec2 tc2;
+	tc2.x = TexCoord.x * 128.0;
+	tc2.y = TexCoord.y * 128.0;
 
-	// vec4 alphaColor = texture(texture2, tc2);
-    gl_FragColor = vec4(1.0,0.0,1.0,0.0); // mix(texture(texture1, TexCoord), alphaColor, alphaColor.a);
+	vec4 alphaColor = texture(texture2, tc2);
+    fragColor = mix(texture(texture1, TexCoord), alphaColor, alphaColor.a);
 }
