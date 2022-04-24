1d3034d621113ea300d2a26681f6c1bf56235eb8
Pinball Power <info@pinball-power.com>
2022-04-24 19:44:14 +0200

    Saving frames implemented, but async not yet ready

--- processor/framestore.cpp
+++ processor/framestore.cpp
@@ -1,15 +1,85 @@
+#include <iomanip>
+
+#include <boost/log/trivial.hpp>
+
 #include "framestore.h"
 
 FrameStore::~FrameStore()
 {
+	outputfile.close();
+	BOOST_LOG_TRIVIAL(debug) << "[framestore] " << frameno << " frames written";
 }
 
 bool FrameStore::configure_from_ptree(boost::property_tree::ptree pt_general, boost::property_tree::ptree pt_source)
 {
-	return false;
+	string name = pt_source.get("filename", "");
+	if (name == "") {
+		BOOST_LOG_TRIVIAL(error) << "filename has not been configured, aborting";
+		return false;
+	}
+
+	ignore_duplicates = pt_source.get("ignore_duplicates", true);
+	async = pt_source.get("async", true);
+
+	outputfile.open(name, ios::out);
+	finished = false;
+
+	return true;
+
 }
+
 
 DMDFrame FrameStore::process_frame(DMDFrame& f)
 {
-	return DMDFrame();
+	if (f.get_bitsperpixel() > 8) {
+		BOOST_LOG_TRIVIAL(debug) << "[framestore] storing colored frames not supported";
+		return f;
+	}
+
+	if (!ignore_duplicates) {
+		uint32_t checksum = f.get_checksum();
+		if (seen.contains(checksum)) {
+			BOOST_LOG_TRIVIAL(trace) << "[framestore] frame seen before, ignoring";
+			return f;
+		}
+		seen.insert(checksum);
+	}
+
+	if (!finished) {
+		if (async) {
+			frames_to_write.push(f);
+		}
+		else {
+			write_to_file(f);
+		}
+	}
+	return f;
+}
+
+void FrameStore::write_to_file(DMDFrame& f)
+{
+	outputfile << "$" << std::hex << std::setw(8) << std::setfill('0') << frameno << "\r\n";
+
+	string line = "";
+	int col = 0;
+	for (auto px : f.get_data()) {
+		line.push_back('0' + (char)px);
+
+		if (col == f.get_width() - 1) {
+			outputfile << line << "\r\n";
+			line = "";
+			col = 0;
+		}
+		else {
+			col++;
+		}
+	}
+
+	assert(col == 0); // there should be no pixel left
+
+	outputfile << "\r\n";
+
+	BOOST_LOG_TRIVIAL(trace) << "[framestore] frame " << frameno << " written";
+
+	frameno++;
 }
--- processor/framestore.h
+++ processor/framestore.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <unordered_set>
+#include <queue>
+
 #include <boost/property_tree/ptree.hpp>
 
 #include "../dmd/dmdframe.h"
@@ -13,4 +18,25 @@ public:
 
 	virtual bool configure_from_ptree(boost::property_tree::ptree pt_general, boost::property_tree::ptree pt_source);
 	virtual DMDFrame process_frame(DMDFrame &f);
+
+private:
+
+	void write_to_file(DMDFrame& f);
+
+	ofstream outputfile;
+	bool finished = true;
+	bool ignore_duplicates = true;
+	bool async = false;
+
+	/// <summary>
+	/// Checksums of frames that have been seen already
+	/// </summary>
+	unordered_set<uint32_t> seen;
+
+	/// <summary>
+	/// Queue of frames that are not yet written
+	/// </summary>
+	queue<DMDFrame> frames_to_write;
+ 
+	int frameno = 1;
 };
