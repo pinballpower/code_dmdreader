1cd18e4b10866c77adba46be0493abc28161866f
Pinball Power <info@pinball-power.com>
2022-04-27 14:51:21 +0200

    Improve formating

--- processor/frameinfologger.cpp
+++ processor/frameinfologger.cpp
@@ -1,3 +1,5 @@
+#include <iomanip>
+
 #include <boost/property_tree/ptree.hpp>
 #include <boost/log/trivial.hpp>
 
@@ -19,6 +21,6 @@ bool FrameInfoLogger::configureFromPtree(boost::property_tree::ptree pt_general,
 
 DMDFrame FrameInfoLogger::processFrame(DMDFrame &f)
 {
-	BOOST_LOG_TRIVIAL(info) << "[frameinfologger] got frame " << f.getWidth() << "x" << f.getHeight() << " " << f.getBitsPerPixel() << "bpp, checksum " << f.getChecksum();
+	BOOST_LOG_TRIVIAL(info) << "[frameinfologger] got frame " << f.getWidth() << "x" << f.getHeight() << " " << f.getBitsPerPixel() << "bpp, checksum " << std::hex << std::setfill('0') << std::setw(8)  << f.getChecksum();
 	return f;
 }
