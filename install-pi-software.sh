afdf6fbf6acfbad1c2b2b2a61be9228e843080cd
HiFiBerry <90598459+pinballpower@users.noreply.github.com>
2022-05-04 10:11:30 +0200

    added libav libraries

--- install-pi-software.sh
+++ install-pi-software.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
 sudo apt install -y iotop ninja-build git libssl-dev
 sudo apt install -y --no-install-recommends raspberrypi-ui-mods lxterminal gvfs
-sudo apt install -y libx11-dev libxcursor-dev libxinerama-dev libxrandr-dev libxi-dev libasound2-dev mesa-common-dev libgl1-mesa-dev libgbm-dev 
+sudo apt install -y libx11-dev libxcursor-dev libxinerama-dev libxrandr-dev libxi-dev libasound2-dev mesa-common-dev libgl1-mesa-dev libgbm-dev libavutil-dev libavformat-dev libavfilter-dev
 sudo apt install -y libstb-dev libglbinding-dev libglfw3-dev libglm-dev libfmt-dev libglew-dev libdrm-dev libboost-all-dev g++ libraspberrypi-dev raspberrypi-kernel-headers
 
