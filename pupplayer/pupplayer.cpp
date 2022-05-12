a81a5b8290c2187360012ab79609310d78c10282
Daniel Matuschek <daniel@matuschek.net>
2022-05-12 22:16:58 +0200

    created a template to read puppack config files

--- pupplayer/pupplayer.cpp
+++ pupplayer/pupplayer.cpp
@@ -3,10 +3,54 @@
 #include <queue>
 
 #include <boost/log/trivial.hpp>
+#include <boost/algorithm/string.hpp>
 
 #include "../drm/drmhelper.hpp"
 #include "../drm/videofile.hpp"
 #include "trigger.hpp"
+
+
+template <class myType>
+vector<myType> readConfigFile(string filename) {
+    vector<myType> result;
+
+    ifstream is;
+    is.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+        is.open(filename);
+    }
+    catch (std::ios_base::failure e) {
+        BOOST_LOG_TRIVIAL(error) << "[trigger] can't open file " << filename << ": " << e.what();
+        return result;
+    }
+
+    bool eof = false;
+    string line;
+    // ignore first line with the field headers
+    std::getline(is, line);
+    while (!eof) {
+        try {
+            if (!std::getline(is, line)) {
+                if (std::cin.eof()) {
+                    eof = true;
+                }
+                else {
+                    throw std::ios_base::failure("couldn't read file");
+                }
+            }
+            boost::trim_right(line);
+
+            myType obj = myType(line);
+            if (obj.isValid()) {
+                result.push_back(obj);
+            }
+        }
+        catch (std::ios_base::failure e) {
+            eof = true;
+        }
+    }
+    return result;
+}
 
 PUPPlayer::~PUPPlayer()
 {
@@ -14,12 +58,12 @@ PUPPlayer::~PUPPlayer()
 
 bool PUPPlayer::configureFromPtree(boost::property_tree::ptree pt_general, boost::property_tree::ptree pt_source)
 {
-	return true;
+ 	return true;
 }
 
 bool PUPPlayer::start()
 {
-	return true
+    return true;
 }
 
 void PUPPlayer::playEvent(int event)
