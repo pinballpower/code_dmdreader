9f50ec18b99685094984030068fe11344618d285
Daniel Matuschek <daniel@matuschek.net>
2022-05-26 10:04:39 +0200

    Add resizing of video files

--- pupplayer/pivid.cpp
+++ pupplayer/pivid.cpp
@@ -1,5 +1,18 @@
 #include "pivid.hpp"
 
+#include <filesystem>
+#include <string>
+
+#include <boost/log/trivial.hpp>
+
+void resizeFile(string filename, int width, int height) {
+	std::filesystem::path p = filename;
+	string ext = p.extension();
+	string basename = p.replace_extension();
+	string newName = basename + "-" + to_string(width) + "x" + to_string(height) + ext;
+	BOOST_LOG_TRIVIAL(info) << "[pivid] converting " << filename << " to " << newName;
+}
+
 string screenToPividJSON(const map<int, PUPScreen> screens)
 {
 	return "";
@@ -17,5 +30,14 @@ bool PividPUPPlayer::stopVideoPlayback(const PUPScreen& screen, bool waitUntilSt
 
 bool PividPUPPlayer::initializeScreens()
 {
-	return false;
+	// prepare videos
+	for (auto screen : screens) {
+		vector<string> files = getFilesForScreen(screen.second.screenNum);
+		for (auto f : files) {
+			resizeFile(f, screen.second.width, screen.second.height);
+		}
+	}
+	return true;
 }
+
+
