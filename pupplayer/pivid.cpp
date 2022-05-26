0f176867d9300d0f477d498ef3980a7d81c22684
Daniel Matuschek <daniel@matuschek.net>
2022-05-26 08:22:53 +0200

    Implement PUPPlayer

--- pupplayer/pivid.cpp
+++ pupplayer/pivid.cpp
@@ -4,3 +4,18 @@ string screenToPividJSON(const map<int, PUPScreen> screens)
 {
 	return "";
 }
+
+bool PividPUPPlayer::startVideoPlayback(string filename, const PUPScreen& screen, bool loop)
+{
+	return false;
+}
+
+bool PividPUPPlayer::stopVideoPlayback(const PUPScreen& screen, bool waitUntilStopped)
+{
+	return false;
+}
+
+bool PividPUPPlayer::initializeScreens()
+{
+	return false;
+}
--- pupplayer/pivid.hpp
+++ pupplayer/pivid.hpp
@@ -4,5 +4,19 @@
 #include <map>
 
 #include "screen.hpp"
+#include "pupplayer.hpp"
 
 string screenToPividJSON(const map<int, PUPScreen> screens);
+
+class PividPUPPlayer : public PUPPlayer {
+
+public:
+
+private:
+	string basedir;
+
+	virtual bool startVideoPlayback(string filename, const PUPScreen& screen, bool loop) override;
+	virtual bool stopVideoPlayback(const PUPScreen& screen, bool waitUntilStopped = true) override;
+	virtual bool initializeScreens() override;
+};
+
