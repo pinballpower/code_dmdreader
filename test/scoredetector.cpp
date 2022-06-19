fbb37c7a62d98218a7cacb68dafb9ae7b5bba41b
Daniel Matuschek <daniel@matuschek.net>
2022-06-19 19:23:39 +0200

    dummy dev code

--- test/scoredetector.cpp
+++ test/scoredetector.cpp
@@ -4,15 +4,38 @@
 
 #include "../dmd/dmdframe.hpp"
 #include "../util/image.hpp"
+#include "../dmd/palette.hpp"
+#include "../processor/patternmatcher.hpp"
 
 void testScoreDetect()
 {
+	vector<uint32_t> pd_4_ffc300_data{
+	0x00000000, 0x100c0000, 0x21190000, 0x32260000,
+	0x43330000, 0x54400000, 0x654d0000, 0x765a0000,
+	0x87670000, 0x98740000, 0xa9810000, 0xba8e0000,
+	0xcb9b0000, 0xdca80000, 0xedb50000, 0xffc30000 };
+	DMDPalette pd_4_ffc300 = DMDPalette(pd_4_ffc300_data, 4, "pd_4_ffc300");
+
 	std::filesystem::current_path("/home/matuschd/code_dmdreader");
-	RGBBuffer rgbbuff = RGBBuffer::fromImageFile("output/frame000001.png");
-	DMDFrame f1 = DMDFrame(rgbbuff);
+	RGBBuffer rgbbuff = RGBBuffer::fromImageFile("output/frame000558.png");
+	DMDFrame f1 = DMDFrame(rgbbuff).removeColors(4, pd_4_ffc300, false);
 
-	rgbbuff = RGBBuffer::fromImageFile("samples/csdigits1-0123456789.png");
-	DMDFrame f2 = DMDFrame(rgbbuff);
+	PatternMatcher pm = PatternMatcher("samples/csdigits1-0123456789.png");
 
-	auto x = f1;
+	rgbbuff = RGBBuffer::fromImageFile("samples/csdigits1-0123456789.png", true);
+	RGBBuffer section = rgbbuff.getRegion(0, 0, 4, 8);
+	DMDFrame f2 = DMDFrame(section);
+
+	f2 = f2.removeColors(4, pd_4_ffc300, true);
+
+	for (int x = 0; x < f1.getWidth() - f2.getWidth(); x++) {
+		for (int y = 0; y < f1.getHeight() - f2.getHeight(); y++) {
+			auto match = pm.matchAt(f1, x, y);
+			if (match.has_value()) {
+				printf("%i,%i: %c\n", x, y, match);
+			}
+		}
+	}
+
+	auto fxx = f1;
 }
