ec1423ff2310b11f05414281fb20ea550180bb0d
Pinball Power <info@pinball-power.com>
2022-10-04 10:25:22 +0100

    Use only one core to avoid memory problems

--- compile-pi.sh
+++ compile-pi.sh
@@ -7,4 +7,4 @@ fi
 INCLUDE_PATH="/usr/include:/usr/include/stb:/usr/include/libdrm:/opt/vc/include"
 export CMAKE_INCLUDE_PATH=$INCLUDE_PATH
 cmake -DCMAKE_BUILD_TYPE=Release -DMAX_LOG_LEVEL=info .
-make -j 4
+make -j 1
