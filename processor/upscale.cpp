ae4fb70bdc01e2b3c7606f4600a1a6bdff4bc4ea
Pinball Power <info@pinball-power.com>
2023-03-14 08:00:06 +0100

    Use the xbr module now

--- processor/upscale.cpp
+++ processor/upscale.cpp
@@ -5,13 +5,15 @@
 // https://en.wikipedia.org/wiki/Pixel-art_scaling_algorithms
 //
 
-extern "C" void hqxInit(void);
-extern "C" void hq2x_32(uint32_t const* src, uint32_t * dest, int width, int height);
-extern "C" void hq3x_32(uint32_t const* src, uint32_t * dest, int width, int height);
-extern "C" void hq4x_32(uint32_t const* src, uint32_t * dest, int width, int height);
+Upscale::~Upscale() {
+	delete(xbrData);
+}
 
 DMDFrame Upscale::processFrame(DMDFrame& f)
 {
+	xbr_params xbrParams;
+	int scaleFactor = 1;
+
 	if (f.getBitsPerPixel() != 32) {
 		BOOST_LOG_TRIVIAL(info) << "[upscale] module requires at least 32bits/pixel input data, doing nothing";
 		return f;
@@ -24,35 +26,44 @@ DMDFrame Upscale::processFrame(DMDFrame& f)
 	switch (upscaler)
 	{
 		case Upscaler::hq2x: 
-			dst_width = src_width * 2; 
-			dst_height = src_height * 2;   
+		case Upscaler::xbr2x:
+			scaleFactor = 2;
 			break;
 		case Upscaler::hq3x: 
-			dst_width = src_width * 3; 
-			dst_height = src_height * 3;   
+		case Upscaler::xbr3x:
+			scaleFactor = 3;
 			break;
 		case Upscaler::hq4x: 
-			dst_width = src_width * 4; 
-			dst_height = src_height * 4;   
+		case Upscaler::xbr4x:
+			scaleFactor = 4;
 			break;
 	}
+
+	dst_width = src_width * scaleFactor;
+	dst_height = src_height * scaleFactor;
+
 
 	// allocate destination temporary buffer
 	uint32_t* dst_data = new uint32_t[dst_width * dst_height];
 
+
+	auto src_data = f.getPixelData();
+	xbrParams.data = xbrData;
+	xbrParams.input = src_data.data();
+	xbrParams.output = (uint8_t*)dst_data;
+	xbrParams.inWidth = src_width;
+	xbrParams.inHeight = src_height;
+	xbrParams.inPitch = src_width * 4;
+	xbrParams.outPitch = src_width * scaleFactor * 4;
+
 	switch (upscaler)
 	{
-		case Upscaler::hq2x:
-			hq2x_32((uint32_t*)f.getPixelData().data(), dst_data, src_width, src_height);
-			break;
-		case Upscaler::hq3x:
-			hq3x_32((uint32_t*)f.getPixelData().data(), dst_data, src_width, src_height);
-			break;
-			break;
-		case Upscaler::hq4x:
-			hq4x_32((uint32_t*)f.getPixelData().data(), dst_data, src_width, src_height);
-			break;
-			break;
+		case Upscaler::hq2x: xbr_filter_hq2x(&xbrParams); break;
+		case Upscaler::hq3x: xbr_filter_hq2x(&xbrParams); break;
+		case Upscaler::hq4x: xbr_filter_hq2x(&xbrParams); break;
+		case Upscaler::xbr2x: xbr_filter_xbr2x(&xbrParams); break;
+		case Upscaler::xbr3x: xbr_filter_xbr3x(&xbrParams); break;
+		case Upscaler::xbr4x: xbr_filter_xbr4x(&xbrParams); break;
 	}
 
 	DMDFrame result = DMDFrame(dst_width, dst_height, 32, (uint8_t*)dst_data);
@@ -62,9 +73,33 @@ DMDFrame Upscale::processFrame(DMDFrame& f)
 }
 
 bool Upscale::configureFromPtree(boost::property_tree::ptree pt_general, boost::property_tree::ptree pt_source) {
-	// TODO: allow different upscalers
-	hqxInit();
-	upscaler = Upscaler::hq2x;
+
+	string upscalerConf = pt_source.get("upscaler", "hq2x");
+
+	if (upscalerConf == "hq2x") {
+		upscaler = Upscaler::hq2x;
+	} else if (upscalerConf == "hq3x") {
+		upscaler = Upscaler::hq3x;
+	}
+	else if (upscalerConf == "hq4x") {
+		upscaler = Upscaler::hq4x;
+	} 
+	else if (upscalerConf == "xbr2x") {
+		upscaler = Upscaler::xbr2x;
+	}
+	else if (upscalerConf == "xbr3x") {
+		upscaler = Upscaler::xbr3x;
+	}
+	else if (upscalerConf == "xbr4x") {
+		upscaler = Upscaler::xbr4x;
+	}
+	else {
+		BOOST_LOG_TRIVIAL(error) << "[upscale] upscaler " << upscalerConf << " unknown, can't use upsampling";
+		return false;
+	}
+
+	xbrData = new xbr_data;
+	xbr_init_data(xbrData);
 
 	return true;
 };
--- processor/upscale.hpp
+++ processor/upscale.hpp
@@ -2,14 +2,20 @@
 
 #include "frameprocessor.hpp"
 
-enum class Upscaler { hq2x, hq3x, hq4x };
+#include "../external/xbr/filters.h"
+
+enum class Upscaler { hq2x, hq3x, hq4x, xbr2x, xbr3x, xbr4x };
 
 class Upscale : public DMDFrameProcessor {
 
 public:
+
+	~Upscale();
+
 	virtual DMDFrame processFrame(DMDFrame& f) override;
 	virtual bool configureFromPtree(boost::property_tree::ptree pt_general, boost::property_tree::ptree pt_source) override;
 
 private:
 	Upscaler upscaler;
+	xbr_data* xbrData = NULL;
 };
