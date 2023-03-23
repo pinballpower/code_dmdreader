4cd6a1c83df53fed134751b5b5480d6d0c3a1fcc
Pinball Power <info@pinball-power.com>
2023-03-23 09:56:45 +0100

    Add more settings

--- render/ledmatrixrenderer.cpp
+++ render/ledmatrixrenderer.cpp
@@ -65,7 +65,38 @@ bool LEDMatrixRenderer::configureFromPtree(boost::property_tree::ptree pt_genera
     width = pt_renderer.get("width", 128);
     height = pt_renderer.get("height", 32);
     pwm_bits = pt_renderer.get("pwm_bits", 8);
-    
+    string rgb_sequence = pt_renderer.get("rgb_sequence", "rgb");
+
+    int multiplexing = pt_renderer.get("multiplexing", 0);
+    if ((multiplexing < 0) || (multiplexing>18)) {
+        BOOST_LOG_TRIVIAL(info) << "[ledmatrixrenderer] multiplexing range is 0-18, ignoring " << multiplexing;
+            multiplexing = 0;
+    }
+
+    int scanmode = pt_renderer.get("scan_mode", 0);
+    if ((scanmode < 0) || (scanmode > 1)) {
+        BOOST_LOG_TRIVIAL(info) << "[ledmatrixrenderer] scanmode can only by 0/1, ignoring " << scanmode;
+        scanmode = 0;
+    }
+
+    int dither_bits = pt_renderer.get("dither_bits", 0);
+    if ((dither_bits < 0) || (dither_bits > 2)) {
+        BOOST_LOG_TRIVIAL(info) << "[ledmatrixrenderer] dither_bits can only by 0-2, ignoring " << dither_bits;
+        dither_bits = 0;
+    }
+
+    int lsb_nanoseconds = pt_renderer.get("lsb_nanoseconds", 100);
+    if ((lsb_nanoseconds < 50) || (lsb_nanoseconds > 300)) {
+        BOOST_LOG_TRIVIAL(info) << "[ledmatrixrenderer] lsb_nanoseconds range is 50-300, ignoring " << lsb_nanoseconds;
+        lsb_nanoseconds = 100;
+    }
+
+    int brightness = pt_renderer.get("brightness", 100);
+    if ((brightness < 0) || (brightness > 100)) {
+        BOOST_LOG_TRIVIAL(info) << "[ledmatrixrenderer] brightness range is 0-100, ignoring " << brightness;
+        brightness = 100;
+    }
+
     // TODO: check what other configuration parameters could be useful
 
     memset(&options, 0, sizeof(options));
@@ -74,6 +105,12 @@ bool LEDMatrixRenderer::configureFromPtree(boost::property_tree::ptree pt_genera
     options.chain_length = 1;
     options.pwm_bits = pwm_bits;
     options.hardware_mapping = "dmdreader";
+    options.led_rgb_sequence = rgb_sequence.c_str();
+    options.scan_mode = scanmode;
+    options.multiplexing = multiplexing;
+    options.pwm_dither_bits = dither_bits;
+    options.pwm_lsb_nanoseconds = lsb_nanoseconds;
+    options.brightness = brightness;
 
     memset(&rt_options, 0, sizeof(rt_options));
     rt_options.gpio_slowdown = 4;
