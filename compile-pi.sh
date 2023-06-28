d14ce29ec2558039f7dae66e10064e86f8f5c540
Pinball Power <info@pinball-power.com>
2023-06-28 12:36:27 +0100

    Force options that should be available on the Pi

--- compile-pi.sh
+++ compile-pi.sh
@@ -12,5 +12,5 @@ fi
 
 INCLUDE_PATH="/usr/include:/usr/include/stb:/usr/include/libdrm:/opt/vc/include"
 export CMAKE_INCLUDE_PATH=$INCLUDE_PATH
-cmake -DCMAKE_BUILD_TYPE=Release -DMAX_LOG_LEVEL=info .
+cmake -DCMAKE_BUILD_TYPE=Release -DMAX_LOG_LEVEL=info -DFORCE_SPI=1 -DFORCE_SERUM=1 -DFORCE_VNI=1 -DFORCE_XBR=1 -DFORCE_LEDMATRIX=1.
 make -j 1
