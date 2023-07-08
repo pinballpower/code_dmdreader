33b286c13ec6f4264876b1f63d2c87b36b43e02c
Pinball Power <info@pinball-power.com>
2023-07-08 07:29:05 +0200

    add some counters

--- processor/ratelimit.cpp
+++ processor/ratelimit.cpp
@@ -1,18 +1,27 @@
 #include "ratelimit.hpp"
 
+#include "../../util/counter.hpp"
+
+#define RL_FRAMES				"ratelimite::frames::all"
+#define RL_FRAMES_ACCEPTED		"ratelimite::frames::accepted"
+#define RL_FRAMES_REJECTED		"ratelimite::frames::rejected"
+
 RateLimit::RateLimit()
 {
 }
 
 DMDFrame RateLimit::processFrame(DMDFrame& f)
 {
+	INC_COUNTER(RL_FRAMES);
 	if (accept_every > 0) {
 		count += 1;
 		if (count >= accept_every) {
 			count = 0;
+			INC_COUNTER(RL_FRAMES_ACCEPTED);
 			return f;
 		}
 		else {
+			INC_COUNTER(RL_FRAMES_REJECTED);
 			return DMDFrame();
 		}
 	}
